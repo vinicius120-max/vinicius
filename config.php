770265a1f78ff8eea91e4117f82ca8736f4f37aa
vinicius120-max <146148695+vinicius120-max@users.noreply.github.com>
2023-09-26 22:37:22 -0300

    Add files via upload

---
+++ config.php
@@ -0,0 +1,22 @@
+<?php
+
+   $dbhost = 'Localhost';
+   $dbUsername = 'root';
+   $dbPassword = '';
+   $dbName = 'vinicius-formulario';
+
+   $conexao = new mysqli($dbhost,$dbUsername,$dbPassword,$dbName);
+
+   //if($conexao->connect_errno)
+  // {
+   //   echo "erro";
+  // }
+  // else
+ //  {
+ //    echo "conexao";
+ //  }
+
+
+
+?>      
+     
---
+++ home.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SITE | CADASTRO</title>
+    <style>
+        body{
+            font-family: arial, Helvetica, sans-serif;
+            background: linear-gradient(to right, rgb(159, 5, 220), rgb(30, 54, 200), rgb(100,52,220));
+            text-align: center;
+            color: white;        
+        }
+        .box{
+            position: absolute;
+            top:50%;
+            left: 50%;
+            transform: translate(-50%,-50%);
+            background-color: rgba(0, 0, 0, 0.6);
+            padding: 30px;
+            border-radius: 15px;
+            
+        }
+        a {
+            text-decoration: none;
+            color: white;
+            border: 3px solid dodgerblue;
+            border-radius: 15px;
+            padding: 10px;
+        }
+        a:hover{
+            background-color: dodgerblue;
+        }
+
+        
+    </style>
+</head>
+<body>
+    <h1>Faça o seu login</h1>
+    <h2>Sistema de cadastro</h2>
+    <div class="box">
+         <a href="login.php">Login</a>
+         <a href="formulario.php">Cadastra-se</a>
+              </div>
+    
+</body>
+</html>
